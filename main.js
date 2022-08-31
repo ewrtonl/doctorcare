d5e5e98f309f3d6b2624590f434267cb50fc72c9
Ewerton Luís <67519714+ewrtonl@users.noreply.github.com>
2022-08-31 16:02:47 -0300

    navgation error fixed

--- main.js
+++ main.js
@@ -13,41 +13,58 @@ function onScroll() {
 }
 
 function activateMenuAtCurrentSection(section) {
+  // linha alvo
   const targetLine = scrollY + innerHeight / 2
 
   // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // topo da seção
   const sectionTop = section.offsetTop
+
+  // altura da seção
   const sectionHeight = section.offsetHeight
-  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
 
   // verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
 
+  // a seção termina onde?
   const sectionEndsAt = sectionTop + sectionHeight
-  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
   // limites da seção
-  const sectionBoundaries =
-    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+  const sectionBoudaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
 
+  // procurando o atributo id dentro da seção
   const sectionId = section.getAttribute('id')
+
+  // pegando o atributo id da seção como string
   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
   menuElement.classList.remove('active')
-  if (sectionBoundaries) {
+
+  if (sectionBoudaries) {
     menuElement.classList.add('active')
+    // console.log(`seção atual: ${sectionId} \nfoi adicionada a classe active!`)
   }
 }
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigationbar.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll')
+    navigationbar.classList.remove('scroll')
   }
 }
 
 function showBackToTopButtonOnScroll() {
-  if (scrollY > 400) {
+  if (scrollY > 700) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
@@ -76,6 +93,11 @@ ScrollReveal({
 #about,
 #about header,
 #about .content,
+#about .content img,
+#contact,
+#contact header,
+#contact .content,
+#contact .content img, 
 #depoiments,
 #depoiments header,
 #depoiments .content,
